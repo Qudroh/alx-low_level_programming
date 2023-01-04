c69685af6a7b542f147f5e65ebd8d90975ff4106
Qudroh <qudroholorunshola@gmail.com>
2023-01-04 13:03:11 -0800

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - sqrt using recursion
+ * @n: parametr to be sqrt
+ * Return: square root of a num
+*/
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
