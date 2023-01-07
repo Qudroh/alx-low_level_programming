76a991b1f7a2c94a0c8bcfd0d2bf0abbe35f5b50
Qudroh <qudroholorunshola@gmail.com>
2023-01-06 16:38:21 -0800

    solved

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - this is a function to print its name
@@ -9,4 +9,5 @@
 int main(int argc _attribute_((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
+	return (0);
 }
