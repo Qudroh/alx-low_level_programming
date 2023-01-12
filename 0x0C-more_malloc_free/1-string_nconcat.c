ed4d61819af94d161eaf40e5faae183106043329
Qudroh <qudroholorunshola@gmail.com>
2023-01-12 14:21:19 -0800

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,23 +4,22 @@
 
 /**
  * _strlen - calculate and return string length
- * @string: str
- * Return: str length
+ * @string: string
+ * Return: string length
 */
 int _strlen(char *string)
 {
 	int i;
 
 	for (i = 0; string[i] != '\0'; i++)
-
-	return (i);
+		return (i);
 }
 /**
- * string_nconcat - concat n bytes of  string to another
- * @s1: str to append to
- * @s2: str to concat from
- * @n: number of bytes from s2 to concat to s1
- * Return: pointer to the resulting string
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -37,11 +36,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	if (num >= _strlen(s2))
 		num = _strlen(s2);
+
 	len = _strlen(s1) + num + 1;
 
 	ptr = malloc(sizeof(*ptr) * len);
 	if (ptr == NULL)
 		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
 	for (j = 0; j < num; j++)
