7946c3cea976054bf37a4caec7b19d8cdebe8b92
Qudroh <qudroholorunshola@gmail.com>
2023-01-20 15:09:10 -0800

    joint

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calc the sum of the int
+ * @a: first int
+ * @b: second int
+ * Return: sum of a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calc the diff of two ints
+ * @a: first int
+ * @b: second int
+ * Return: differences of a and b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calc the producr of two ints
+ * @a: first int
+ * @b: second int
+ * Return: the product of a and b
+*/
+int op_mul(int a, int  b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calc the div of two ints
+ * @a: first int
+ * @b: second int
+ * Return: division of a and b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calc the remainder of the div of two ints
+ * @a: first int
+ * @b:second int
+ * Return: remainder of a divided b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
