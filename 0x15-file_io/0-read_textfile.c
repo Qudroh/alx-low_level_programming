98f8373ad9c5441492bafac24038f0e305260c5b
Qudroh <qudroholorunshola@gmail.com>
2023-02-08 23:50:23 -0800

    hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,13 +15,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	o - open(filename, 0_RDONLY);
-	r - read(o, buffer, letters);
-	w - write(STDOUT_FILENO, buffer, r);
+	o = open(filename, 0_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
